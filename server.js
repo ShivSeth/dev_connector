b7b40f87e6da5edfa7f88d58548989d8555d5dc4
ShivSeth <shivam.prakash114@gmail.com>
2021-03-14 00:14:23 +0530

    express routes

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ app.get("/", (req, res) => {
   res.send("API running");
 });
 
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
+
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
